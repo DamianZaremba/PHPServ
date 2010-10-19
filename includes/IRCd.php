9d5a962b902743944c14e49f2018ec0c72d74546
snofox <snofox@cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1>
2010-10-19 04:43:00 +0000

    More abstract functions and a dash of organization
    git-svn-id: https://cobihome.external.cluenet.org:8443/svn/phpserv/trunk@244 cb14c9df-8a02-4fc1-9b40-3ccfc4edb2c1

--- includes/IRCd.php
+++ includes/IRCd.php
@@ -9,28 +9,35 @@
  * 		--SnoFox
  */
 abstract class IRCd {
+	/* Standard user functions */
+	protected abstract function raw ( $string );
 	public abstract function kick( PHPServBot $from, Channel $channel, User $user, $reason );
 	public abstract function mode( PHPServBot $from, Channel $channel, Mode $mode );
-	public abstract function privmsg( PHPServBot $from, User $user, $message);
-	// "Prefix" as in /msg @#clueirc --SnoFox
-	public abstract function chanmsg( PHPServBot $from, Channel $channel, $message, $prefix = null );
-	// /msg $*.cluenet.org Hi world! --SnoFox
-	public abstract function servmsg( PHPServBot $from, $serverMask, $message);
-	public abstract function ctcp( PHPServBot $from, User $user, $type, $message );
-	public abstract function ctcpreply( PHPServBot $from, User $user, $type, $message);
-	public abstract function notice( PHPServBot $from, User $user, $message);
-	public abstract function chanNotice( PHPServBot $from, Channel $channel, $message, $prefix = null );
-	public abstract function kill( PHPServBot $from, User $user, $message);
 	public abstract function join( PHPServBot $from, Channel $channel);
 	public abstract function part( PHPServBot $from, Channel $channel, $message = '');
 	public abstract function invite( PHPServBot $from, User $user, Channel $channel);
 	public abstract function topic( PHPServBot $from, Channel $channel, $newTopic);
+	public abstract function nick( PHPServBot $from, $to, $timestamp = time());
+	public abstract function quit( PHPServBot $from, $message);
+	// Messages
+	public abstract function privmsg( PHPServBot $from, User $user, $message);
+	public abstract function chanmsg( PHPServBot $from, Channel $channel, $message, $prefix = null );
+	public abstract function ctcp( PHPServBot $from, User $user, $type, $message );
+	public abstract function ctcpreply( PHPServBot $from, User $user, $type, $message);
+	public abstract function notice( PHPServBot $from, User $user, $message);
+	public abstract function chanNotice( PHPServBot $from, Channel $channel, $message, $prefix = null );
+	/* Oper functions */
+	public abstract function servmsg( PHPServBot $from, $serverMask, $message);
+	public abstract function kill( PHPServBot $from, User $user, $message);
 	public abstract function wallops( PHPServBot $from, $message);
 	public abstract function squit( PHPServBot $from, $servMask);
 	public abstract function kline( PHPServBot $from, $banMask, $expiry, $reason);
-	// Is this legal? Or necessary? (the timestamp) :P --SnoFox
-	public abstract function nick( PHPServBot $from, $to, $timestamp = time());
-	public abstract function quit( PHPServBot $from, $message);
-	protected abstract function raw ( $string );
+	/* Server functions */
+	public abstract function addNick( $nick, $identd, $address, $gecos);
+	
+	/* Other random odds and ends */
+	public abstract function isValidNick( $nick );
+	public abstract function isValidHost( $hostname );
+	 
 }
 ?>
